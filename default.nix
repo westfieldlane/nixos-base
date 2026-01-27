79557f17d87d81af0a75526ccb42adeb56b7a71c
user9592844 <139545179+user9592844@users.noreply.github.com>
2026-01-27 10:49:15 +0800

    Fix: Add autoUpgrade.nix back into configuration

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@
   # Add all core software that every machine should have
   # NOTE: the services sub-directory sandboxes services appropriately in SystemD
   imports = [
+    ./autoUpgrade.nix
     ./bat.nix
     ./bottom.nix
     ./fd.nix
